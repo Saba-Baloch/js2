94614cec2cdbc6b0b8148ce53929a6e744346b1a
Saba-Baloch <156021281+Saba-Baloch@users.noreply.github.com>
2024-03-21 23:51:04 +0430

    added index.html file

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+// function marks(om,tm) {
+//     return(
+//         om+tm
+        
+//     )
+    
+// };
+function marks(om) {
+    document.write(
+        om/1100*100
+    );
+    
+}
+let name1 ="Aleena, "
+
+document.write(name1);
+
+
+
+
+
+
+
+
+marks(900); 
+
+prompt("type your name here " );
+
